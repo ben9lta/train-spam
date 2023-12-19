d842fb45a2a1b9b82e8067a3dec1ef5ac98a8461
Александр Бондарь <bondar.9lta@gmail.com>
2023-12-19 21:11:22 +0300

    removed public/build for hosting

---
+++ public/build/assets/SpamTypeHelper-2257c8a0.js
@@ -0,0 +1,1 @@
+const n=[0,1,2,3];function s(t){switch(t){case 0:return"Не спам";case 1:return"Спам";case 2:return"Рекламный";case 3:return"Другой";default:return"Не определено"}}export{n as L,s as t};
